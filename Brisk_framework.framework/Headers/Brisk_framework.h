50e2d636615644ccfad7929f5f44f54b30537d64
Nermin Sehic <nerminsehic@Nermins-MacBook-Pro.local>
2021-12-15 21:13:36 +0100

    Initial deployment of Brisk static framework

---
+++ Brisk_framework.framework/Headers/Brisk_framework.h
@@ -0,0 +1,18 @@
+//
+//  Brisk_framework.h
+//  Brisk framework
+//
+//  Created by Nermin Sehic on 15. 12. 2021..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Brisk_framework.
+FOUNDATION_EXPORT double Brisk_frameworkVersionNumber;
+
+//! Project version string for Brisk_framework.
+FOUNDATION_EXPORT const unsigned char Brisk_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Brisk_framework/PublicHeader.h>
+
+
